b345834740c53fc2bf3d75ba609f421970e5159b
Ayush-Jadhav <ayujadhav121@gmail.com>
2025-12-15 07:54:05 +0530

    make frontend url dynamic

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ connectWithDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
